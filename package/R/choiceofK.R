c807094732ec328678a672316fd5efa0fad680d0
Yuchao Jiang <yuchaojiang@Yuchaos-MacBook-Pro.local>
2017-12-19 01:09:34 -0500

    removed old package

--- package/R/choiceofK.R
+++
@@ -1,9 +0,0 @@
-choiceofK <- function(AIC, BIC, RSS, K, filename) {
-    Kmax <- length(AIC)
-    pdf(filename, width = 13, height = 4)
-    par(mfrow = c(1, 3))
-    plot(K, RSS, type = "b", xlab = "Number of latent variables")
-    plot(K, AIC, type = "b", xlab = "Number of latent variables")
-    plot(K, BIC, type = "b", xlab = "Number of latent variables")
-    dev.off()
-}
