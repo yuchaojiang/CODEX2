9bf58bf448827e2b4b74ed85726c0438713cba01
Brad Wubbenhorst <bwubbenhorst@gmail.com>
2021-02-24 12:24:52 -0500

    Update getbambed.R
    This sort of ref can change the order of ref compared to the bedFile, at least in the targeted pipeline. When the gene column of bedFile is cbind to ref, it will cause the genes to be mapped incorrectly. I see no reason to sort the ref; alternatively you can sort the bedFile the same way as ref each time you read it.

--- package/R/getbambed.R
+++ package/R/getbambed.R
@@ -4,6 +4,6 @@ getbambed = function (bamdir, bedFile, sampname, projectname)
     stop("Please check the bed file directory provided. File could not be \n            found!")
   exomtarg <- read.table(bedFile, sep = "\t")
   ref <- GRanges(seqnames=exomtarg[,1],ranges=IRanges(start=exomtarg[,2], end=exomtarg[,3]))
-  ref <- sort(ref)
+  #ref <- sort(ref)
   list(bamdir = bamdir, sampname = sampname, ref = ref, projectname = projectname)
 }
